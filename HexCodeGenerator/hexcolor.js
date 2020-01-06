6f8b79fd4871cf2c570aaa1c2b337077c73dcce3
Oseghr <oseghr@outlook.com>
2020-01-06 12:30:41 -0700

    update to simple applications built from studying and practice

--- HexCodeGenerator/hexcolor.js
+++ HexCodeGenerator/hexcolor.js
@@ -1,8 +1,10 @@
+//target element/id/class
 const bodyBcg = document.querySelector("body");
 const hexBtn = document.querySelector('.hexBtn');
 const hex = document.querySelector('.hex');
 const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
+//add event listeners
 hexBtn.addEventListener('click', getHex);
 
 function getHex() {
